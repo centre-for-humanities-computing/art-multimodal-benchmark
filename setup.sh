462784ab14e9edc1dd536857abf21235b187c3ef
Louise Brix Pilegaard Hansen <louise.bp@hotmail.com>
2025-08-18 09:59:37 +0200

    update setup + notebook

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # create virtual environment
-python3 -m venv env
+/opt/homebrew/bin/python3.11 -m venv env
 
 # activate env
 source ./env/bin/activate
@@ -11,6 +11,6 @@ pip install -r requirements.txt
 pip install ipykernel
 
 # install kernel so env can be used in jupyter notebooks
-python -m ipykernel install --user --name=benchmark_env
+python -m ipykernel install --user --name=benchmark_env # need to restart vscode to use!
 
 deactivate 
