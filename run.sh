9af32f272d4c8e803da952457547715c444ab427
Louise Brix Pilegaard Hansen <louise.bp@hotmail.com>
2025-08-28 10:58:58 +0200

    add download script

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 source ./env/bin/activate
 
-python3 src/run_mieb.py --leaderboard mieb_leaderboard.csv --n_models 4 --dataset wiki_subset --label_cols genre style artist --epochs 3 --hidden_layer_size 1200 --batch_size 32 --log_file_name testy3
+python3 src/download_dataset.py --hf_name huggan/wikiart --local_name wikiart
+
+#python3 src/run_mieb.py --leaderboard mieb_leaderboard.csv --n_models 4 --dataset wiki_subset --label_cols genre style artist --epochs 3 --hidden_layer_size 1200 --batch_size 32 --log_file_name testy3
 
 deactivate
